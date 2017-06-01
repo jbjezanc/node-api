b5de99119a8c22bdc0671f2a3d55124069faa416
jbjezanc <josip.bjezancevic@gmail.com>
2017-06-01 09:53:49 +0200

    Add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,7 +1,5 @@
-// #8a
 const {ObjectId} = require('mongodb');
 
-// #1
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
@@ -21,31 +19,28 @@ User.findById(id).then(user => {
 });
 
 // var id = '592fa60198e7ab03cd7390cf';
-// var id = '692fa60198e7ab03cd7390cf'; // #5 increment the first num of the id.
-// var id = '592fa60198e7ab03cd7390cf111';    // #7b invalid format for the id
+// var id = '692fa60198e7ab03cd7390cf';
+// var id = '592fa60198e7ab03cd7390cf111';
 
-// #8b
 // if (!ObjectId.isValid(id)) {
 //   console.log('ID not valid');
 // }
 
 // Todo.find({
-//   _id: id   // #2 converts string id to ObjectId
+//   _id: id
 // }).then((todos) => {
 //   console.log('Todos', todos);
 // });
 
-// #3
 // Todo.findOne({
 //   _id: id
 // }).then((todo) => {
 //   console.log('Todo', todo);
 // });
 
-// #4
 // Todo.findById(id).then(todo => {
 //   if (!todo) {
 //     return console.log('Id not found');
 //   }
 //   console.log('Todo By Id', todo);
-// }).catch(e => console.log(e)); // #7a
+// }).catch(e => console.log(e));
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+// Challenge - #c1:
+const {ObjectId} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 
 var {Todo} = require('./models/todo');
@@ -28,6 +31,32 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// #1a GET /todos/1234
+ // #1b creates id var on the req object that we can access.
+app.get('/todos/:id', (req, res) => {
+  // res.send(req.params);
+  var id = req.params.id;
+
+  // #c2 - Valid id using isValid
+  if (!ObjectId.isValid(id)) {
+    // 404 - send back empty send
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      // no todo - send back 404 with empty body
+      return res.status(404).send();
+    }
+    // success, todo find - send back todo
+    res.status(200).send({todo}); // send todo as a property of an object - that later can be modified
+  }).catch(e => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('App started on port 3000');
